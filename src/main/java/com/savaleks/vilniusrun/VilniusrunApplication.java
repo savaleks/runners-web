38acd8c7e8299c7014e55064682cf3c90cc85d6f
Alexander <savaleks@gmail.com>
2019-03-13 11:43:24 +0200

    Initial commit for the new project

---
+++ src/main/java/com/savaleks/vilniusrun/VilniusrunApplication.java
@@ -0,0 +1,13 @@
+package com.savaleks.vilniusrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilniusrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VilniusrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savaleks/vilniusrun/VilniusrunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.savaleks.vilniusrun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VilniusrunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
